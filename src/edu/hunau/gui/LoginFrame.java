1d78e96059373c1a426754386e6295872cceb711
zerolxy <1056180778@qq.com>
2022-06-11 11:18:39 +0800

    'BoxLayout登录界面实现'

--- src/edu/hunau/gui/LoginFrame.java
+++ src/edu/hunau/gui/LoginFrame.java
@@ -1,11 +1,12 @@
 package edu.hunau.gui;
 
-import java.awt.BorderLayout;
-
 import javax.swing.Box;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
+import java.awt.Font;
 
 import edu.hunau.gui.utils.BaseFrame;
 
@@ -16,21 +17,63 @@ import edu.hunau.gui.utils.BaseFrame;
  */
 public class LoginFrame extends BaseFrame {
 	public void init() {
+		this.setTitle("登录");
 		Box y_repeat = Box.createVerticalBox();
-		Box x_repeat = Box.createHorizontalBox();
 		
-		JLabel userNameLabel = new JLabel();
-		x_repeat.add(new JLabel("用户名"));
-		x_repeat.add(new JTextField(20));
-		y_repeat.add(Box.createVerticalStrut(25));
+		y_repeat.add(Box.createVerticalStrut(5));
 		
-		this.setSize(300,200);
+		JPanel titlePanel = new JPanel();
+		JLabel titleLabel = new JLabel("登录服务器");
+		titleLabel.setFont(new Font("黑体",Font.BOLD,30));
+		titlePanel.add(titleLabel);
+		y_repeat.add(titlePanel);
+		y_repeat.add(Box.createVerticalStrut(5));
 		
-		this.add(x_repeat,BorderLayout.CENTER);this.add(y_repeat,BorderLayout.SOUTH);
+		
+		JPanel userNamePanel = new JPanel();
+		JLabel userNameLabel = new JLabel("用户名");
+		JTextField userNameField = new JTextField(20);
+		userNamePanel.add(userNameLabel);
+		userNamePanel.add(userNameField);
+		
+		y_repeat.add(userNamePanel);
+		y_repeat.add(Box.createVerticalStrut(15));
+		
+		JPanel hostPanel = new JPanel();
+		JLabel hostLabel = new JLabel("主机名");
+		JTextField hostField = new JTextField(20);
+		hostPanel.add(hostLabel);
+		hostPanel.add(hostField);
+		
+		y_repeat.add(hostPanel);
+		y_repeat.add(Box.createVerticalStrut(5));
+		
+		JPanel portPanel = new JPanel();
+		JLabel portLabel = new JLabel("端口号");
+		JTextField portField = new JTextField(20);
+		portPanel.add(portLabel);
+		portPanel.add(portField);
+		
+		y_repeat.add(portPanel);
+		y_repeat.add(Box.createVerticalStrut(5));
+		
+		JPanel btnPanel = new JPanel();
+		JButton restBtn = new JButton("重置");
+		JButton connBtn = new JButton("连接");
+		btnPanel.add(restBtn);
+		btnPanel.add(connBtn);
+		
+		y_repeat.add(btnPanel);
+		
+		this.setSize(350,280);
+		
+		this.add(y_repeat);
 		this.setVisible(true);
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	
+	
 	public static void main(String[] args) {
 		new LoginFrame().init();
 	}
