9c9b16b55a8525b461f8016089d19fd5bb4a7978
zerolxy <1056180778@qq.com>
2022-06-12 20:23:50 +0800

    'Server的简单测试'

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,9 +1,40 @@
 package server;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**     
 *  
 * @author 龙翔宇  
 * @date 2022年6月12日  新建  
 */
 public class Server {
-
+	private ServerSocket serverSocket;
+	
+	public Server() {
+		try {
+			serverSocket = new ServerSocket(8088);
+			int port = serverSocket.getLocalPort();
+			String hostName = InetAddress.getLocalHost().getHostAddress();
+			System.out.println("服务器启动,等待客户端连接"+ hostName + ",监听端口" + port);
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void start() {
+//		监听客户端
+		try {
+//			如果有客户端连接,会返回一个socket实例
+			Socket clientSocket= serverSocket.accept();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public static void main(String[] args) {
+		new Server().start();
+	}
 }
