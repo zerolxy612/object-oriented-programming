44807b80b89beb8bb8968e8b6faa311645d950fc
zerolxy <1056180778@qq.com>
2022-06-12 21:03:17 +0800

    '绑定ServerFrame传递数据'

--- src/edu/hunau/gui/ServerFrame.java
+++ src/edu/hunau/gui/ServerFrame.java
@@ -24,86 +24,92 @@ import javax.swing.table.DefaultTableModel;
 import edu.hunau.gui.utils.BaseFrame;
 import server.Server;
 
-/**     
-*  
-* @author 龙翔宇  
-* @date 2022年6月11日  新建  
-*/
-public class ServerFrame extends BaseFrame{
-	
+/**
+ * 
+ * @author 龙翔宇
+ * @date 2022年6月11日 新建
+ */
+public class ServerFrame extends BaseFrame {
+
+	private JTextArea messageArea;
+
 	public void init() {
-		this.setSize(500,450);
-		
-		this.add(topPanel(),BorderLayout.NORTH);
+		this.setSize(500, 450);
+
+		this.add(topPanel(), BorderLayout.NORTH);
 		this.add(middlePanel());
-		this.add(bottomPanel(),BorderLayout.SOUTH);
-		
+		this.add(bottomPanel(), BorderLayout.SOUTH);
+
 		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 //	底部的组件
 	private JPanel bottomPanel() {
-		JPanel jPanel =  new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		
+		JPanel jPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
 		JTextField messageField = new JTextField(15);
-		messageField.setPreferredSize(new Dimension(0,28));
-		
+		messageField.setPreferredSize(new Dimension(0, 28));
+
 		JButton sendBtn = new JButton("发送");
-		
+
 		jPanel.add(messageField);
 		jPanel.add(sendBtn);
 		return jPanel;
 	}
-	
-	
-//	中间的组件
+
+    public JTextArea getMessageArea() {
+		return messageArea;
+	}
+
+	//	中间的组件
 	private Component middlePanel() {
-		JPanel jPanel =  new JPanel();
-		
+		JPanel jPanel = new JPanel();
+
 //		获得分割组件为水平摆放的切分面板
 		JSplitPane jSplitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
-		
+
 		jSplitPane.setDividerSize(5);
 		jSplitPane.setDividerLocation(150);
 //		设置切分线位置不可移动
 		jSplitPane.setEnabled(false);
-		
-	    JScrollPane leftPane = new JScrollPane();
+
+		JScrollPane leftPane = new JScrollPane();
 //	    表格中的数据模型
 //	    提供字段名称
-	    Vector<String> colNames = new Vector<>();
-	    colNames.add("客户端名称");
-	    
-	    DefaultTableModel clientInfoTableModel = new DefaultTableModel(colNames,0);
-	    JTable clientTable = new JTable(clientInfoTableModel);
+		Vector<String> colNames = new Vector<>();
+		colNames.add("客户端名称");
+
+		DefaultTableModel clientInfoTableModel = new DefaultTableModel(colNames, 0);
+		JTable clientTable = new JTable(clientInfoTableModel);
 		leftPane.setViewportView(clientTable);
-	    
-        jSplitPane.setLeftComponent(leftPane);
-        
-        JScrollPane rightPane = new JScrollPane();
-		
-		JTextArea messageArea = new JTextArea();
+
+		jSplitPane.setLeftComponent(leftPane);
+
+		JScrollPane rightPane = new JScrollPane();
+
+		// ctrl+1
+		messageArea = new JTextArea();
 		rightPane.setViewportView(messageArea);
-		
+
 		jSplitPane.setRightComponent(rightPane);
-		
+
 		return jSplitPane;
 	}
 
 //	顶部的组件
 	private JPanel topPanel() {
 		JPanel jPanel = new JPanel();
-		
+
 		JLabel jLabel = new JLabel("服务器端口");
 		JTextField portField = new JTextField();
-		portField.setPreferredSize(new Dimension(50,28));
-		
+		portField.setPreferredSize(new Dimension(50, 28));
+
 //		设置内容在文本框居中
 		portField.setHorizontalAlignment(JTextField.CENTER);
 //		设置默认值
 		portField.setText("8088");
-		
+
 //		设置按钮的图标
 		JButton excuteBtn = new JButton(new ImageIcon("start.png"));
 		// TODO: 启动服务器
@@ -112,33 +118,33 @@ public class ServerFrame extends BaseFrame{
 			public void actionPerformed(ActionEvent e) {
 				String btnStatus = excuteBtn.getIcon().toString();
 				if (btnStatus.startsWith("start")) {
-					//启动服务器 Server必须由线程进行处理
-					new Thread(new Server()).start();
-					//设置图标
+					// 启动服务器 Server必须由线程进行处理
+					new Thread(new Server(ServerFrame.this)).start();
+					// 设置图标
 					excuteBtn.setIcon(new ImageIcon("stop.png"));
-				}else {
-					//关闭服务器
+				} else {
+					// 关闭服务器
 					excuteBtn.setIcon(new ImageIcon("start.png"));
 				}
-				
+
 			}
 		});
-		
+
 		jPanel.add(jLabel);
 		jPanel.add(portField);
 		jPanel.add(excuteBtn);
-		
+
 		return jPanel;
 	}
-	
+
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable(){
-			
+		EventQueue.invokeLater(new Runnable() {
+
 			@Override
 			public void run() {
 				new ServerFrame().init();
 			}
 		});
-		
+
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -5,6 +5,10 @@ import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.swing.JOptionPane;
+
+import edu.hunau.gui.ServerFrame;
+
 /**     
 *  
 * @author 龙翔宇  
@@ -12,6 +16,19 @@ import java.net.Socket;
 */
 public class Server implements Runnable{
 	private ServerSocket serverSocket;
+	
+	private ServerFrame serverFrame;
+//	this.serverFrame = serverFrame;
+	
+	public Server(ServerFrame serverFrame) {
+		try {
+			serverSocket = new ServerSocket(8088);
+			this.serverFrame = serverFrame;
+			
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 	public Server() {
 		try {
@@ -27,7 +44,7 @@ public class Server implements Runnable{
 
 	}
 	public static void main(String[] args) {
-		new Server().start();
+//		new Server().start();
 	}
 
 	@Override
@@ -37,8 +54,12 @@ public class Server implements Runnable{
 			
 			int port = serverSocket.getLocalPort();
 			String hostName = InetAddress.getLocalHost().getHostAddress();
+			String message = "服务器启动成功,ip地址为"+ hostName + ",监听端口:" + port;
 			System.out.println("服务器启动,等待客户端连接"+ hostName + ",监听端口" + port);
+			// 弹出一个消息框
+			JOptionPane.showMessageDialog(null, message);
 			
+			serverFrame.getMessageArea().append(message);
 //			如果有客户端连接,会返回一个socket实例
 			Socket clientSocket= serverSocket.accept();
 		} catch (IOException e) {
