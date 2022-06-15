79ed6af932bbd9a34841fd9dc53d116bee20e18d
zerolxy <1056180778@qq.com>
2022-06-15 23:49:49 +0800

    '客户端登录信息显示在服务端'

--- src/eud/huanu/gui/server/Server.java
+++ src/eud/huanu/gui/server/Server.java
@@ -81,14 +81,17 @@ public class Server implements Runnable{
 			serverFrame.getMessageArea().append(ChatRoomUtils.showMessage(message));
 //			如果有客户端连接,会返回一个socket实例
 			Socket clientSocket= serverSocket.accept();
+			String clientIp = clientSocket.getLocalAddress().getHostAddress();
 			System.out.println("服务端:获得客户端连接");
+			serverFrame.getMessageArea().append(ChatRoomUtils.showMessage("获得客户端连接,ip地址为: "+clientIp));
 			
 			// TODO: 读取客户端的数据
 			InputStream is = clientSocket.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is,Charset.forName("utf-8"));
 			BufferedReader br = new BufferedReader(isr);
 			String data = br.readLine();
-			System.out.println("服务端:"+data);
+			// TODO 需要进行修改
+			serverFrame.getMessageArea().append(ChatRoomUtils.showMessage(data+"登录上线!"));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
